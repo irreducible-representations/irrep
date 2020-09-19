0209589add4122a80b2922c23d27ec5fb6906973
Matthew Horton <m.k.horton@gmail.com>
2020-09-18 18:59:40 -0700

    Add example stdout for tests

--- examples/Bi-scalar/run.sh
+++ examples/Bi-scalar/run.sh
@@ -1,1 +1,1 @@
-irrep -Ecut=50 -code=abinit -fWFK=Bi_WFK -refUC=0,-1,1,1,0,-1,-1,-1,-1 -kpoints=11 -IBend=5 -kpnames="GM"
+irrep -Ecut=50 -code=abinit -fWFK=Bi_WFK -refUC=0,-1,1,1,0,-1,-1,-1,-1 -kpoints=11 -IBend=5 -kpnames="GM" > out
