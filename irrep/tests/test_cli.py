8e8ed42c790cdc939ddb31cb8e3f002532d7ffc9
Matthew Horton <m.k.horton@gmail.com>
2020-09-19 14:25:10 -0700

    Add second test

--- irrep/tests/test_cli.py
+++ irrep/tests/test_cli.py
@@ -5,7 +5,7 @@ from pathlib import Path
 TEST_FILES_PATH = Path(__file__).parents[2] / "examples"
 
 
-def test_Bi_scalar_example():
+def test_bi_scalar_example():
 
     os.chdir(TEST_FILES_PATH / "Bi-scalar")
 
@@ -20,14 +20,36 @@ def test_Bi_scalar_example():
         "-kpnames=GM",
     ]
 
+    output = subprocess.run(command, capture_output=True, text=True)
+
+    with open("out") as f:
+        reference_out = f.read()
+
+    assert output.returncode == 0
+    assert output.stdout.strip() == reference_out.strip()
+
     for test_output_file in (
-        "bands-.dat",
-        "bands-sym-.dat",
         "irreps.dat",
         "irreptable-template",
         "trace.txt",
     ):
         os.remove(test_output_file)
+
+
+def test_bi_spinor_example():
+
+    os.chdir(TEST_FILES_PATH / "Bi-spinor")
+
+    command = [
+        "irrep",
+        "-Ecut=50",
+        "-code=abinit",
+        "-fWFK=Bi_WFK",
+        "-refUC=0,-1,1,1,0,-1,-1,-1,-1",
+        "-kpoints=11",
+        "-IBend=5",
+        "-kpnames=GM",
+    ]
 
     output = subprocess.run(command, capture_output=True, text=True)
 
@@ -36,3 +58,10 @@ def test_Bi_scalar_example():
 
     assert output.returncode == 0
     assert output.stdout.strip() == reference_out.strip()
+
+    for test_output_file in (
+        "irreps.dat",
+        "irreptable-template",
+        "trace.txt",
+    ):
+        os.remove(test_output_file)
