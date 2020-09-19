2ee2278024566b9a91fb69b306070b05c424c3ba
Matthew Horton <m.k.horton@gmail.com>
2020-09-19 14:29:08 -0700

    Add third test (failing)
    Gives:
    
    ```
    Atom type indices:
     [2, 1]
    ```
    
    Instead of reference:
    
    ```
     Atom type indices:
     [1, 2]
    ```

--- irrep/tests/test_cli.py
+++ irrep/tests/test_cli.py
@@ -65,3 +65,34 @@ def test_bi_spinor_example():
         "trace.txt",
     ):
         os.remove(test_output_file)
+
+
+def test_wannier_spin_example():
+
+    os.chdir(TEST_FILES_PATH / "wannier-spin")
+
+    command = [
+        "irrep",
+        "-code=wannier90",
+        "-prefix=PbTe",
+        "-kpoints=1,33,37,419",
+        "-Ecut=30",
+        "-kpnames=GM,L,X,W",
+        "-refUC=-1,+1,-1,-1,+1,+1,+1,+1,-1"
+    ]
+    output = subprocess.run(command, capture_output=True, text=True)
+
+    with open("out") as f:
+        reference_out = f.read()
+
+    assert output.returncode == 0
+    assert output.stdout.strip() == reference_out.strip()
+
+    for test_output_file in (
+        "bands-.dat",
+        "bands-sym-.dat",
+        "irreps.dat",
+        "irreptable-template",
+        "trace.txt",
+    ):
+        os.remove(test_output_file)
