fix 2 bugs. Bug-1: parsing .win when length units ommited. Bug-2: convert xred from dtype=str to dtype=float

irrep/bandstructure.py
@@ -370,7 +370,7 @@ class BandStructure:
                         L = [next(iterwin) for i in range(3)]
                     else:
                         units = "ang"
-                        L = l1 + [next(iterwin)[0] for i in range(2)]
+                        L = [l1] + [next(iterwin) for i in range(2)]
                     self.Lattice = np.array(L, dtype=float)
                     if units == "bohr":
                         self.Lattice *= BOHR
@@ -415,7 +415,7 @@ class BandStructure:
                         xred.append(l1[1:4])
                     typatdic = {n: i + 1 for i, n in enumerate(set(nameat))}
                     typat = [typatdic[n] for n in nameat]
-                    xred = np.array(xred)
+                    xred = np.array(xred, dtype=float)
                     if l[1][6:10] == "cart":
                         xred = xred.dot(np.linalg.inv(self.Lattice))
 