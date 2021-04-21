c0cef98fffae9ff99e44fa0cb014f22121429171
MIraola <mikeliraola95@gmail.com>
2021-04-21 22:23:09 +0200

    remove some UNK, to save space

--- examples/wannier-spinors-NiAs/run.sh
+++ examples/wannier-spinors-NiAs/run.sh
@@ -1,2 +1,2 @@
-irrep -code=wannier90 -prefix=NaAs -Ecut=50 -kpnames=GM,Z,X,R,M,A -kpoints=1,2,3,4,7,8 > out
+irrep -code=wannier90 -prefix=NaAs -Ecut=50 -kpnames=GM,A -kpoints=1,8 > out
 rm trace.txt irreps.dat irreptable-template
