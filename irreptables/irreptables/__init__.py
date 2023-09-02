61098b4a2a8d78cc543ab3716d9f6bafbdac3e7a
Stepan Tsirkin <stepan.tsirkin@uzh.ch>
2023-09-03 01:07:26 +0200

    actually included tables

--- irreptables/irreptables/__init__.py
+++ irreptables/irreptables/__init__.py
@@ -14,7 +14,7 @@
 ##  e-mail: stepan.tsirkin@physik.uzh.ch                         #
 ##################################################################
 
-__version__="1.0.2"
+__version__="1.0.3"
 
 import copy
 import os
