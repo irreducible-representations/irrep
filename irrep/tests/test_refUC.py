09f38e93a6120a5b145b65c85ba4b2738bf2d54b
MIraola <mikeliraola95@gmail.com>
2023-09-28 18:42:12 +0200

    add comparisons for more quantities in tests and compare arrays, floats, bools,... instead of strings when possible

--- irrep/tests/test_refUC.py
+++ irrep/tests/test_refUC.py
@@ -1,6 +1,8 @@
 import os
 import subprocess
 from pathlib import Path
+from monty.serialization import dumpfn, loadfn
+import numpy as np
 
 TEST_FILES_PATH = Path(__file__).parents[2] / "examples"
 
@@ -15,69 +17,61 @@ def test_bi_hoti():
         "-code=vasp",
         "-kpnames=T,GM,F,L",
         "-Ecut=50",
-        "-refUC=1,-1,0,0,1,-1,1,1,1",
         "-EF=5.2156",
         "-IBstart=5",
         "-IBend=10"
     ]
     output = subprocess.run(command, capture_output=True, text=True)
-
     return_code = output.returncode
-    stdout = output.stdout
-
-    with open("test_out", "w") as f:
-        f.write(stdout)
-
     assert return_code == 0, output.stderr
-    assert "-2.7306  |        2      | -GM8(1.0)" in stdout, stdout
-    assert "-0.7762  |        2      | -GM8(1.0)" in stdout, stdout
-    assert "-0.4961  |        2      | -GM4(1.0), -GM5(1.0)" in stdout, stdout
-    assert "-4.8263  |        2      | -F3(1.0), -F4(1.0)" in stdout, stdout
-    assert "-3.6784  |        2      | -F5(1.0), -F6(1.0)" in stdout, stdout
-    assert "-2.4303  |        2      | -F5(1.0), -F6(1.0)" in stdout, stdout
-    assert "-1.7054  |        2      | -L5(1.0), -L6(1.0)" in stdout, stdout
-    assert "-1.6885  |        2      | -L3(1.0), -L4(1.0)" in stdout, stdout
-    assert "-0.1312  |        2      | -L5(1.0), -L6(1.0)" in stdout, stdout
-    assert "-1.5597  |        2      | -T9(1.0)" in stdout, stdout
-    assert "-1.2220  |        2      | -T8(1.0)" in stdout, stdout
-    assert "0.1460  |        2      | -T6(1.0), -T7(1.0)" in stdout, stdout
 
-    # Test without specifying refUC in CLI
-    command = [
-        "irrep",
-        "-spinor",
-        "-code=vasp",
-        "-kpnames=T,GM,F,L",
-        "-Ecut=50",
-        "-EF=5.2156",
-        "-IBstart=5",
-        "-IBend=10"
-    ]
-    output = subprocess.run(command, capture_output=True, text=True)
+    # Load generated and reference output data
+    data_ref = loadfn("ref_output.json")
+    data_run = loadfn("irrep-output.json")
 
-    return_code = output.returncode
-    stdout = output.stdout
+    # Check SpaceGroup
+    sg_ref = data_ref['spacegroup']
+    sg_run = data_run['spacegroup']
+    assert sg_ref['name'] == sg_run['name']
+    assert sg_ref['number'] == sg_run['number']
+    assert sg_ref['spinor'] == sg_run['spinor']
+    assert sg_ref['num_symmetries'] == sg_run['num_symmetries']
+    assert sg_ref['cells_match'] == sg_run['cells_match']
+    spinor = sg_ref['spinor']  # used later
 
-    with open("test_out", "w") as f:
-        f.write(stdout)
+    # Todo: implement safe check of symmetries
 
-    assert return_code == 0, output.stderr
-    assert "-2.7306  |        2      | -GM8(1.0)" in stdout, stdout
-    assert "-0.7762  |        2      | -GM8(1.0)" in stdout, stdout
-    assert "-0.4961  |        2      | -GM4(1.0), -GM5(1.0)" in stdout, stdout
-    assert "-4.8263  |        2      | -F3(1.0), -F4(1.0)" in stdout, stdout
-    assert "-3.6784  |        2      | -F5(1.0), -F6(1.0)" in stdout, stdout
-    assert "-2.4303  |        2      | -F5(1.0), -F6(1.0)" in stdout, stdout
-    assert "-1.7054  |        2      | -L5(1.0), -L6(1.0)" in stdout, stdout
-    assert "-1.6885  |        2      | -L3(1.0), -L4(1.0)" in stdout, stdout
-    assert "-0.1312  |        2      | -L5(1.0), -L6(1.0)" in stdout, stdout
-    assert "-1.5597  |        2      | -T9(1.0)" in stdout, stdout
-    assert "-1.2220  |        2      | -T8(1.0)" in stdout, stdout
-    assert "0.1460  |        2      | -T6(1.0), -T7(1.0)" in stdout, stdout
+    # Check general properties of the band structure
+    bs_ref = data_ref['characters_and_irreps'][0]['subspace']
+    bs_run = data_run['characters_and_irreps'][0]['subspace']
+    assert bs_ref['indirect gap (eV)'] == bs_run['indirect gap (eV)']
+    assert bs_ref['Minimal direct gap (eV)'] == bs_run['Minimal direct gap (eV)']
+    if spinor:
+        assert bs_ref['Z4'] == bs_run['Z4']
+        assert bs_ref['number of inversion-odd Kramers pairs'] == bs_run['number of inversion-odd Kramers pairs']
+    else:
+        assert bs_ref['number of inversion-odd states'] == bs_run['number of inversion-odd states']
 
+
+    # Check properties at each k-point
+    kp_ref = bs_ref['k-points'][0]
+    kp_run = bs_run['k-points'][0]
+    assert np.allclose(kp_ref['symmetries'], kp_run['symmetries'])
+    assert np.allclose(kp_ref['energies'], kp_run['energies'], rtol=0., atol=1e-4)
+    assert np.allclose(kp_ref['characters'], kp_run['characters'], rtol=0., atol=1e-4)
+    assert kp_ref['characters_refUC_is_the_same'] == kp_run['characters_refUC_is_the_same']
+    assert np.allclose(kp_ref['dimensions'], kp_run['dimensions'], rtol=0., atol=1e-4)
+    for irrep_ref, irrep_run in zip(kp_ref['irreps'], kp_run['irreps']):
+        assert len(irrep_ref) == len(irrep_run)
+        for irrepname_ref, irrepname_run in zip(irrep_ref.keys(), irrep_run.keys()):
+            assert irrepname_ref == irrepname_run  # compare strings of irreps
+            assert np.allclose(irrep_ref[irrepname_ref], irrep_run[irrepname_run])  # compare multiplicities
+
+    # Remove output files created during run
     for test_output_file in (
             "irreps.dat",
             "irreptable-template",
             "trace.txt",
+            "irrep-output.json"
     ):
         os.remove(test_output_file)
