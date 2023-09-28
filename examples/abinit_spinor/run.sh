9db34e978895ba553df62e42d226cd4acf65f8a7
MIraola <mikeliraola95@gmail.com>
2023-09-28 18:40:39 +0200

    force automatic search of refUC in abinit examples and update output files

--- examples/abinit_spinor/run.sh
+++ examples/abinit_spinor/run.sh
@@ -1,2 +1,2 @@
-irrep -spinor -Ecut=50 -code=abinit -fWFK=O_DS2_WFK -refUC=0,-1,1,1,0,-1,-1,-1,-1 -kpoints=1 -IBstart=21 -IBend=32 -kpnames="GM" > out
+irrep -spinor -Ecut=50 -code=abinit -fWFK=O_DS2_WFK -kpoints=1 -IBstart=21 -IBend=32 -kpnames="GM" > out
 rm trace.txt irreps.dat irreptable-template
