correct magic formula for coreps

irrep/kpoint.py
@@ -664,9 +664,11 @@ class Kpoint:
                 for ch in self.char:
                     multiplicities = {}
                     for ir in irreptable:
-                        multipl = np.dot(np.array([irreptable[ir][sym.ind] for sym in self.little_group]),
-                                         ch.conj()
-                                         ) / len(ch)
+                        ir_characters = np.array([irreptable[ir][sym.ind] for sym in self.little_group])
+                        # some coreps are not normalized
+                        # this is len(ch) for all irreps
+                        normalization = (ir_characters**2).sum()
+                        multipl = np.dot(ir_characters, ch.conj()) / normalization
                         if abs(multipl) > 1e-3:
                             multiplicities[ir] = multipl
                     irreps.append(multiplicities)