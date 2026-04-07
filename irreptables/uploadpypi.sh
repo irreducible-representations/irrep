25ffe3d2b1b4abdbacb596861e192bdb5b19dd02
Stepan Tsirkin <stepan.tsirkin@epfl.ch>
2026-04-07 15:23:34 +0200

    irreptables-3.1.0

--- irreptables/uploadpypi.sh
+++ irreptables/uploadpypi.sh
@@ -1,5 +1,5 @@
 rm dist/*
 rm */irrep.egg-info  */irreptables.egg-info  build
-python3 -m build
-python3 -m twine upload  -u __token__ dist/*
+python -m build
+python -m twine upload  -r irreptables  dist/*
 
