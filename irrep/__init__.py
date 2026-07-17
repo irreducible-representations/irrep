166f0cbd62db5283850d46a077665ad7d24296f9
Stepan Tsirkin <stepan.tsirkin@epfl.ch>
2026-07-17 14:57:21 +0200

    publish workflow (#145)

--- irrep/__init__.py
+++ irrep/__init__.py
@@ -1,2 +1,5 @@
-__version__ = "3.0.1"
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
 from .parsers.parse_files_bandstructure import parse_files as parse_bandstructure
